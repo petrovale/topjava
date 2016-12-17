43115c47cc9d35a00539b1efa4c537d3b8dfa816
petrovale <leha.isakov@yandex.ru>
2016-12-17 21:52:53 +0300

    HW1

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by user on 15.12.2016.
+ */
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.info("getAll");
+        request.setAttribute("mealList",
+                MealsUtil.getWithExceeded(MealsUtil.MEAL_LIST, 2000));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+
+    }
+}
